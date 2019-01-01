5d1dcef82b9d23d38e78fb87549257e9e4076dfa
Johannes Kühlewindt <johannes.kuehlewindt@gmail.com>
2019-01-02 00:23:40 +0100

    added simplex noise

--- src/mapcreator.js
+++ src/mapcreator.js
@@ -11,6 +11,10 @@
 //     [0,0,0,0,0,0,0,0]
 // ]
 
+const SimplexNoise = require('simplex-noise')
+const simplex = new SimplexNoise()
+
+
 const input = [
     [0,1,0,0],
     [0,1,0,0],
@@ -22,30 +26,9 @@ console.log(createMap(input))
 
 function createMap(input) {
     let result = JSON.parse(JSON.stringify(input))
-    // result = result.map(row => row.map(cell => {
-    //     return Math.floor(Math.random() * 10)
-    // }))
-    result = [ 
-        [ 1, 9, 2, 5 ], 
-        [ 3, 0, 3, 9 ], 
-        [ 3, 8, 6, 6 ], 
-        [ 6, 6, 2, 6 ] 
-    ]
-
     result = result.map((row, i) => row.map((cell, j) => {
-        if (i % 2 || j % 2) {
-            return  ( cell + result[2 * Math.floor(i / 2)][2 * Math.floor(j / 2)] ) / 2
-        }
-        return cell
+        return Math.pow((1 + simplex.noise2D(i, j)) / 2, .5) * 10
     }))
-
-    result = result.map((row, i) => row.map((cell, j) => {
-        if (i % 2 || j % 2) {
-            return  ( cell + result[2 * Math.floor(i / 2)][2 * Math.floor(j / 2)] ) / 2
-        }
-        return cell
-    }))
-
 
     return result 
 }
