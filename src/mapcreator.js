66d69768f75dc07ed1a9c94b89a02d5e582091be
Johannes Kühlewindt <johannes.kuehlewindt@gmail.com>
2019-01-02 01:05:29 +0100

    generate pngs of random generated noise

--- src/mapcreator.js
+++ src/mapcreator.js
@@ -1,34 +1,54 @@
-// const input = [
-//     [0,0,0,0,0,0,0,0],
-//     [0,0,0,0,0,0,0,0],
-//     [0,1,1,1,1,1,1,0],
-//     [0,1,1,0,0,0,1,0],
-//     [0,1,1,0,0,0,1,0],
-//     [0,1,1,1,1,1,1,0],
-//     [0,0,0,1,1,0,0,0],
-//     [0,0,0,1,1,0,0,0],
-//     [0,1,1,1,1,1,1,0],
-//     [0,0,0,0,0,0,0,0]
-// ]
-
-const SimplexNoise = require('simplex-noise')
-const simplex = new SimplexNoise()
-
-
 const input = [
-    [0,1,0,0],
-    [0,1,0,0],
-    [0,1,0,0],
-    [0,1,0,0]
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
 ]
 
-console.log(createMap(input))
+const SimplexNoise = require('simplex-noise')
+const simplex = new SimplexNoise('seed2')
+const Jimp = require('jimp')
 
+// const input = [
+//     [0,1,0,0],
+//     [0,1,0,0],
+//     [0,1,0,0],
+//     [0,1,0,0]
+// ]
+
+console.log(createMap(input))
+createImage(createMap(input))
 function createMap(input) {
     let result = JSON.parse(JSON.stringify(input))
     result = result.map((row, i) => row.map((cell, j) => {
-        return Math.pow((1 + simplex.noise2D(i, j)) / 2, .5) * 10
+        return Math.pow((1 + simplex.noise2D(i * 10, j * 10)) / 2, 1.4) * 10
     }))
 
-    return result 
+    return result
+}
+
+function createImage(input) {
+    var Jimp = require('jimp')
+
+    const width = input.length
+    const height = input[0].length
+    new Jimp(width, height, '#FF00FF', (err, image) => {
+        input.forEach((row, i) => {
+            row.forEach((cell, j) => {
+                var value = cell * 255/10
+                image.setPixelColor(
+                    Jimp.rgbaToInt(value, value, value, 255), 
+                    i, j
+                )
+            })
+        });
+        image.resize(width * 100, height * 100, Jimp.RESIZE_NEAREST_NEIGHBOR)
+        image.write('image.png')
+    })    
 }
